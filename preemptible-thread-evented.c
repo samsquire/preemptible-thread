37caf1445379c4b2de5cb2e9f3815a0cb219c0c6
Samuel Squire <sam@samsquire.com>
2022-06-26 06:46:00 +0100

    Sam file IO executed on threads

--- preemptible-thread-evented.c
+++ preemptible-thread-evented.c
@@ -1,13 +1,17 @@
- #include <pthread.h>
- #include <string.h>
- #include <stdio.h>
- #include <stdlib.h>
- #include <unistd.h>
- #include <errno.h>
- #include <ctype.h>
- #include <time.h>
- #include <math.h>
+#include <pthread.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <ctype.h>
+#include <time.h>
+#include <math.h>
 #include <limits.h>
+#include <sys/types.h> 
+#include <sys/stat.h> 
+#include <fcntl.h> 
+#include <stdatomic.h>
 
 #define READ 0
 #define WRITE 1
@@ -28,19 +32,20 @@ struct ringbuffer {
   int num_io_operations;
   int size;
   struct io_operation **operations;
+  int stopped;
 };
 
 struct lightweight_thread;
 struct thread_info;
 struct lightweight_thread {
   int thread_num;
-  volatile int preempted;
+  volatile atomic_int preempted;
   int num_loops;
   int *limit;
   volatile int *value;
   int *remembered;
   int kernel_thread_num;
-  struct lightweight_thread* (*user_function) (struct lightweight_thread*);
+  struct lightweight_thread* (*user_function) (struct thread_info*, struct lightweight_thread*);
   struct io_thread* io_threads;
   int num_io_threads;
   volatile int tail;
@@ -62,11 +67,11 @@ struct thread_info {    /* Used as argument to thread_start() */
 
 struct io_operation {
   int bytes_returned;
-  FILE* fd;
+  int fd;
   void (*callback) (struct io_operation*, struct ringbuffer*);
   int type;
   int buffer_size;
-  char* buffer;
+  char *buffer;
 
 
 };
@@ -121,6 +126,7 @@ void
 		 */
 		while (__builtin_expect(tp->head >= (rb->last_tail + rb->size), 0))
 		{
+		  printf("Blocking during push..\n");
       
 			int min = rb->tail;
       // printf("%d min %d tail %d last tail\n", min, tp->head, rb->last_tail);
@@ -153,18 +159,19 @@ void
 			if (tp->head < (rb->last_tail + rb->size)) {
         
 				break;
-      }
+		    }
 			
 		}
     // free(&tp->operations[tp->head]);
-    printf("Pushing operation\n");
+    // printf("Pushing operation\n");
 
     rb->operations[tp->head] = ptr;
-    printf("Successfully pushed");
+    // printf("Successfully pushed");
 		// Allow consumers eat the item.
 		tp->head = INT_MAX;
 	}
 
+// def pop
 struct io_operation*
 pop(struct io_thread* io_thread)
 	{
@@ -179,7 +186,7 @@ pop(struct io_thread* io_thread)
 		 * se we don't need a memory barrier here.
 		 */
 		tp->tail = rb->tail;
-    printf("Got tail \n");
+    // printf("Got tail \n");
 		tp->tail = __atomic_fetch_add(&rb->tail, 1, __ATOMIC_SEQ_CST);
     tp->tail = tp->tail % io_thread->ringbuffer->size;
     rb->tail = rb->tail % io_thread->ringbuffer->size;
@@ -192,6 +199,7 @@ pop(struct io_thread* io_thread)
 		 */
 		while (__builtin_expect(tp->tail >= (rb->last_head), 0))
 		{
+      // printf("Blocking during pop %d %d..\n", tp->tail, rb->last_head);
 			int min = rb->head;
     // printf("%d min %d tail %d last head\n", min, io_thread->tail, rb->last_head);
 			// Update the last_head_.
@@ -223,40 +231,18 @@ pop(struct io_thread* io_thread)
 			if (tp->tail < (rb->last_head)) {
         
 				break;
-        }
+        	}
+
+			if (rb->stopped) { return -1; }
 
 		}
-  printf("Returning popped item %d\n", tp->tail);
 		struct io_operation *ret = (tp->ringbuffer->operations[tp->tail]);
-    printf("Found popped item\n");
+		printf("Returning popped item %d\n", tp->tail);
 		// Allow producers rewrite the slot.
 		tp->tail = INT_MAX;
 		return ret;
 	}
 
-int
-io_thread(void *arg) {
-  struct io_thread *io_thread = arg;
-  while (io_thread->running == 1) {
-    printf("IO thread running\n");
-    struct io_operation *io_operation = pop(io_thread);
-    printf("Processing popped item\n");
-    if (io_operation->type == WRITE) {
-      
-    }
-    if (io_operation->type == READ) {
-      printf("Operation type is a READ\n");
-      // fread(&num, sizeof(struct threeNum), 1, fptr);
-      io_operation->bytes_returned = read(io_operation->fd->_fileno, &io_operation->buffer, io_operation->buffer_size);
-       if (!io_operation->bytes_returned) {
-         handle_error("read io thread");
-       }
-      
-      (io_operation->callback)(io_operation, io_thread->ringbuffer);
-    }
-  }
-  return 0;
-}
 
 
 
@@ -269,6 +255,7 @@ struct timer_thread {
   int lightweight_threads_num;
   int delay;
   volatile int running;
+  struct ringbuffer *ringbuffer;
 };
 
 static void *
@@ -307,14 +294,23 @@ do {
 // printf("Time taken %d seconds %d milliseconds (%d iterations)\n",
 //  msec/1000, msec%1000, iterations);
   }
+  // stop all lightweight functions
+  for (int i = 0 ; i < timer_thread->num_threads; i++) {
+    for (int j = 0 ; j < timer_thread->all_threads[i].lightweight_threads_num; j++) {
+      // printf("Preempting kernel thread %d user thread %d\n", i, j);
+      timer_thread->all_threads[i].user_threads[j].preempted = 0;
+      
+    }
+  }
+   
   return 0;
 }
 
  /* Thread start function: display address near top of our stack,
     and return upper-cased copy of argv_string. */
 
- static void *
- thread_start(void *arg)
+static void *
+thread_start(void *arg)
  {
      struct thread_info *tinfo = arg;
      char *uargv;
@@ -330,8 +326,9 @@ do {
          *p = toupper(*p);
        
        }
-
+	
     while (tinfo->running == 1) {
+	 printf("Running kernel thread...\n");
       for (int i = 0 ; i < tinfo->lightweight_threads_num; i++) {
         tinfo->user_threads[i].preempted = 0;
         
@@ -342,12 +339,10 @@ do {
            tinfo->user_threads[previous].preempted = 0;
          }
         tinfo->user_threads[i].preempted = 1;
-        tinfo->user_threads[i].user_function(&tinfo->user_threads[i]);
-         previous = i;
+        tinfo->user_threads[i].user_function(&tinfo, &tinfo->user_threads[i]);
+        previous = i;
       } 
     }
-
-   
      return uargv;
  }
 
@@ -362,27 +357,68 @@ register_loop(int index, int value, struct lightweight_thread* m, int limit) {
   }
 }
 
+// def io_thread()
 int
-do_io(struct io_operation* io_operation, struct ringbuffer* ringbuffer) {
-  printf("IO Callback\n");
-  printf("BUFFER %s\n", &io_operation->buffer);
-  // free(io_operation->buffer);
-  // fclose(io_operation->fd);
-  
+io_thread(void *arg) {
+  struct io_thread *io_thread = arg;
+  while (io_thread->running == 1) {
+    printf("IO thread running\n");
+    struct io_operation *io_operation = pop(io_thread);
+	if (io_operation == -1) {
+		io_thread->running = 0;
+		break;
+	}
+    printf("Processing popped item\n");
+    if (io_operation->type == WRITE) {
+      
+    }
+    if (io_operation->type == READ) {
+      printf("Operation type is a READ\n");
+      // io_operation->bytes_returned = fread(&io_operation->buffer, sizeof(char), io_operation->buffer_size, io_operation->fd);
+	  // printf("File no is %d\n", fileno(io_operation->fd));
+	  printf("Buffer size is %d\n", io_operation->buffer_size);
+       io_operation->bytes_returned = read(io_operation->fd, &io_operation->buffer, io_operation->buffer_size);
+	  printf("Read %d bytes\n", io_operation->bytes_returned);
+       if (!io_operation->bytes_returned || io_operation->bytes_returned == -1) {
+         handle_error("read io thread");
+       }
+      
+      (io_operation->callback)(io_operation, io_thread->ringbuffer);
+    }
+  }
   return 0;
 }
 
+void
+do_io(struct io_operation* io_operation, struct ringbuffer* ringbuffer) {
+  // printf("IO Callback\n");
+  printf("BUFFER %s\n", &io_operation->buffer);
+  // printf("Printed buffer\n");
+  // free(io_operation->buffer);
+  // printf("Freed buffer\n");
+  printf("Need to free %d\n", io_operation->fd);
+  int close_result = close(io_operation->fd);
+  if (close_result == -1) {
+	handle_error("close failure");	
+  }
+  // printf("Successfully closed file\n");
+  free(io_operation);
+}
+
+// def lightweight_thread
 void*
-lightweight_thread_function(struct lightweight_thread* m)
+lightweight_thread_function(struct thread_info* t, struct lightweight_thread* m)
 {
     
-    while (m->preempted != 0) {
-      register_loop(0, 0, m, 100000000);
+    while (t->running == 1 && m->preempted == 1) {
+      register_loop(0, 0, m, 10000000);
+	  printf("Lightweight function running...%d\n", m->preempted);	
       for (; m->value[0] < m->limit[0]; m->value[0]++) {
-        
+      	printf("Doing work\n");  
         sqrt(m->value[0]);
-        char *buffer = calloc(1024, sizeof(char));
-        printf("Allocated buffer");
+        char *buffer = (char*)malloc(1024 * sizeof(char));
+        memset(buffer, '\0', 1024 * sizeof(char));
+        // printf("Allocated buffer\n");
         if (buffer == NULL) {
           handle_error("buffer calloc");
         }
@@ -390,17 +426,27 @@ lightweight_thread_function(struct lightweight_thread* m)
         if (!operations) {
           handle_error("could not allocate operations");
         }
+		if (operations == NULL) {
+		 handle_error("green thread calloc operations");
+		}
         operations->type = READ;
         operations->buffer = buffer;
-        operations->buffer_size = 1024;
-        printf("Opening file");
-        operations->fd = fopen("data", "r");
         
-     if (operations == NULL) {
-         handle_error("green thread calloc operations");
-     }
+        operations->buffer_size = 1024;
+        // printf("Opening file\n");
+		
+        operations->fd = open("/home/sam/preemptible-thread/data", O_RDONLY);
+       	if (operations->fd == NULL || operations->fd == -1 || operations->fd == 0) {
+          // handle_error("could not open file for reading");
+		 // printf("Freeing operations");
+		 free(operations);	
+		 // printf("Freeing buffer");
+		 free(buffer);
+		 break;
+		}
+		// printf("%p is pointer to *FILE\n", operations->fd);
         operations->callback = do_io;
-        printf("Pushing read operation\n");
+        // printf("Pushing read operation\n");
         push(m, operations);
       }
       // printf("Kernel thread %d User thread %d ran\n", m->kernel_thread_num, m->thread_num);
@@ -414,14 +460,14 @@ lightweight_thread_function(struct lightweight_thread* m)
 struct lightweight_thread*
 create_lightweight_threads(int kernel_thread_num, int num_threads, struct ringbuffer* ringbuffer, struct io_thread* io_threads, int size, struct thread_info* tinfo) {
 struct lightweight_thread *lightweight_threads = 
-  calloc(num_threads, sizeof(*lightweight_threads));
+  calloc(num_threads, sizeof(struct lightweight_thread));
        if (lightweight_threads == NULL)
            handle_error("calloc lightweight threads");
   for (int i = 0 ; i < num_threads ; i++) {
     lightweight_threads[i].kernel_thread_num = kernel_thread_num;
     lightweight_threads[i].thread_num = i;
     lightweight_threads[i].num_loops = 1;
-    lightweight_threads[i].all_producers = &tinfo;
+    lightweight_threads[i].all_producers = tinfo; // TODO &tinfo
     lightweight_threads[i].ringbuffer = ringbuffer;
     lightweight_threads[i].io_threads = io_threads;
     lightweight_threads[i].num_io_threads = num_threads;
@@ -459,6 +505,7 @@ struct lightweight_thread *lightweight_threads =
 
      stack_size = 16384ul;
      num_threads = 5;
+	 int number_threads = num_threads;
    
      while ((opt = getopt(argc, argv, "t:")) != -1) {
          switch (opt) {
@@ -502,17 +549,17 @@ struct lightweight_thread *lightweight_threads =
      if (tinfo == NULL)
          handle_error("calloc tinfo");
     for (int tnum = 0 ; tnum < num_threads; tnum++) {
-      tinfo[tnum].running = 1;
+      tinfo[tnum].running++;
     }
 
-struct ringbuffer *io_ringbuffer = calloc(1, sizeof(*io_ringbuffer));
+struct ringbuffer *io_ringbuffer = calloc(1, sizeof(struct ringbuffer));
    io_ringbuffer->size = PENDING_IO_SIZE;
-   io_ringbuffer->last_head = 0;
-   io_ringbuffer->last_tail = 0;
+   io_ringbuffer->last_head = -1;
+   io_ringbuffer->last_tail = -1;
    io_ringbuffer->tail = 0;
    io_ringbuffer->head = 0;
-   struct io_operation *operations = calloc(100, sizeof(struct io_operation));
-   io_ringbuffer->operations = &operations;
+   struct io_operation **operations = calloc(100, sizeof(*operations));
+   io_ringbuffer->operations = operations;
      if (operations == NULL) {
          handle_error("calloc operations");
      }
@@ -530,14 +577,16 @@ struct io_thread *io_thread_info = calloc(io_threads, sizeof(struct io_thread));
       io_thread_info[tnum].ringbuffer = io_ringbuffer;
       io_thread_info[tnum].num_io_threads = io_threads;
       io_thread_info[tnum].thread_num = tnum;
+     
     }
    printf("Set up io_thread_info\n");
 
 
    
-   struct timer_thread *timer_info = calloc(1, sizeof(*timer_info));
+   struct timer_thread *timer_info = calloc(1, sizeof(struct timer_thread));
      timer_info->running = 1;
-      timer_info->delay = 10;
+     timer_info->delay = 10;
+	 timer_info->ringbuffer = io_ringbuffer;
      timer_info->num_threads = num_threads;
       
      if (timer_info == NULL)
@@ -561,10 +610,12 @@ struct io_thread *io_thread_info = calloc(io_threads, sizeof(struct io_thread));
             corresponding element of tinfo[]. */
        }
    for (int tnum = 0; tnum < num_threads; tnum++) {
+		 printf("Creating kernel thread...\n");
          s = pthread_create(&tinfo[tnum].thread_id, &attr,
                             &thread_start, &tinfo[tnum]);
          if (s != 0)
-             handle_error_en(s, "pthread_create");
+             handle_error_en(s, "tinfo pthread_create");
+		
      }
    for (int tnum = 0 ; tnum < io_threads ; tnum++) {
       s = pthread_create(&io_thread_info[tnum].thread_id, &io_thread_attr,
@@ -578,42 +629,59 @@ struct io_thread *io_thread_info = calloc(io_threads, sizeof(struct io_thread));
    s = pthread_create(&timer_info[0].thread_id, &timer_attr,
                             &timer_thread_start, &timer_info[0]);
          if (s != 0)
-             handle_error_en(s, "pthread_create");
+             handle_error_en(s, "timer thread pthread_create");
 
      /* Destroy the thread attributes object, since it is no
         longer needed. */
 
   
 
-     s = pthread_attr_destroy(&attr);
-     if (s != 0)
-         handle_error_en(s, "pthread_attr_destroy");
-     s = pthread_attr_destroy(&timer_attr);
-     if (s != 0)
-         handle_error_en(s, "pthread_attr_destroy timer");
 
      /* Now join with each thread, and display its returned value. */
 
-   s = pthread_join(timer_info->thread_id, &timer_result);
+	 s = pthread_join(timer_info->thread_id, &timer_result);
          if (s != 0)
              handle_error_en(s, "pthread_join");
 
-         printf("Joined timer thread");
+	 printf("Joined timer thread\n");
+	 printf("NUMBER THREADS %d", number_threads);
+	 io_ringbuffer->stopped = 1;
    
+	 printf("Stopping user threads\n");
+	 for (int i = 0 ; i < number_threads; i++) {
+		tinfo[i].running = 0;
+		
+		for (int j = 0 ; j < tinfo[i].lightweight_threads_num; j++) {
+			printf("user thread is %d\n", tinfo[i].user_threads[j].preempted);
+			tinfo[i].user_threads[j].preempted = 0; // stop the lightweight thread	
+			printf("Stopping lightweight thread\n");
+			printf("Number of loops %d\n", tinfo[i].user_threads[j].num_loops);
+			for (int loop = 0 ; loop < tinfo[i].user_threads[j].num_loops; loop++) {
+				printf("Stopping loop\n");
+				tinfo[i].user_threads[j].value[loop] = tinfo[i].user_threads[j].limit[loop];
+			}
+		}
+	 } 
 
-     for (int tnum = 0; tnum < num_threads; tnum++) {
-          tinfo[tnum].running = 0;
-         s = pthread_join(tinfo[tnum].thread_id, &res);
+	 printf("Stopping kernel threads...\n");	
+	 printf("Num threads %d", number_threads);	
+     for (int tnum = 0; tnum < number_threads; tnum++) {
+         tinfo[tnum].running = 0;
+		 void* thread_result;
+		 printf("Waiting for kernel thread to end...\n"); 
+         s = pthread_join(tinfo[tnum].thread_id, &thread_result);
          if (s != 0)
              handle_error_en(s, "pthread_join");
 
-         printf("Joined with thread %d; returned value was %s\n",
+          printf("Joined with thread %d; returned value was %s\n",
                  tinfo[tnum].thread_num, (char *) res);
-         free(res);      /* Free memory allocated by thread */
-       for (int user_thread_num = 0 ; user_thread_num < num_threads; user_thread_num++) {
-         
-           free(tinfo[tnum].user_threads[user_thread_num].remembered);
-           free(tinfo[tnum].user_threads[user_thread_num].value);
+         // free(res);      
+		  // free(tinfo[tnum].argv_string);
+		
+       for (int user_thread_num = 0 ; user_thread_num < number_threads; user_thread_num++) {
+ 		 printf("Freeing remembered value limit\n"); 
+         free(tinfo[tnum].user_threads[user_thread_num].remembered);
+         free(tinfo[tnum].user_threads[user_thread_num].value);
          free(tinfo[tnum].user_threads[user_thread_num].limit);
          
        }
@@ -622,14 +690,25 @@ struct io_thread *io_thread_info = calloc(io_threads, sizeof(struct io_thread));
 
     for (int i = 0 ; i < io_threads; i++) {
         void* io_thread_result;
-        s = pthread_join(io_thread_info->thread_id, &io_thread_result);
+		
+        s = pthread_join(io_thread_info[i].thread_id, &io_thread_result);
          if (s != 0)
              handle_error_en(s, "io thread pthread_join");
 
-         printf("Joined io thread");
+         printf("Joined io thread\n");
+		
    }
+     s = pthread_attr_destroy(&attr);
+     if (s != 0)
+         handle_error_en(s, "pthread_attr_destroy");
+     s = pthread_attr_destroy(&timer_attr);
+     if (s != 0)
+         handle_error_en(s, "pthread_attr_destroy timer");
+     s = pthread_attr_destroy(&io_thread_attr);
+     if (s != 0)
+         handle_error_en(s, "pthread_attr_destroy io thread");
    
-    
+     free(operations); 
      free(io_thread_info);
      free(io_ringbuffer);
      free(timer_info);
